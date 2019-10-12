5f72d33df72161fff8a5d41c2ff6ba55a1f41dd4
Joe <joeholley@me.com>
2019-10-12 03:51:34 +0000

    backing up for the night

--- app/controllers/destresses_controller.rb
+++ app/controllers/destresses_controller.rb
@@ -1,4 +1,7 @@
 class DestressesController < ApplicationController
   def index
   end
+
+  def images
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'destresses#index'
+  get 'images', to: 'destresses#images'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

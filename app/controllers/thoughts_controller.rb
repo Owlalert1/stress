c0929683fb15f442488404c03745b363da092362
Joe <joeholley@me.com>
2019-11-01 22:29:14 +0000

    troubleshooting display of thoughts page

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,2 +1,20 @@
 class ThoughtsController < ApplicationController
+  def index  
+    @thought = Thought.order.first
+  end
+
+  def new
+    @thought = Thought.new
+  end
+
+  def create
+    Thought.create(thought_params)
+  end
+
+  private
+
+  def thought_params
+    params.require(:thought).permit(:thoughts, :name)
+  end
 end
+
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,2 +1,4 @@
 class Thought < ApplicationRecord
+  validates :thoughts, presence: true, length: { maximum: 3000, minimum: 3 }
+  validates :name, presence: true, length: { maximum: 70, minimum: 3 }
 end

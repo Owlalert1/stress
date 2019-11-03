84870c0587b3e1ff240a1be4716abc30f24db392
Joe <joeholley@me.com>
2019-11-03 02:12:19 +0000

    troubleshooting heroku push error have run heroku db migrate

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,5 +1,5 @@
 class Thought < ApplicationRecord
-  has_many :thoughts
+  
   validates :thoughts, presence: true, length: { maximum: 3000, minimum: 3 }
   validates :name, presence: true, length: { maximum: 70, minimum: 3 }
 end
